b9606d77a49b1fd3e496705a404871f67625bf91
Branden <branden.ackley@gmail.com>
2022-08-23 16:02:04 -0700

    added a hash with all letters with symbol number pairs

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,3 +1,11 @@
-def caesar_cipher(string)
+def caesar_cipher(string, shift_num)
+  hash = {
+    A: 0, B: 1, C: 2, D: 3, E: 4, F: 5, G: 6, H: 7, I: 8, J: 9, K: 10,
+    L: 11, M: 12, N: 13, O: 14, P: 15, Q: 16, R: 17, S: 18, T: 19, U: 20,
+    V: 21, W: 22, X: 23, Y: 24, Z: 25
+  }
+  string = string.upcase!
+  array = string.split('')
 
+  string
 end
