5777739ec005da76cf4b672d442f67ee224679e0
Branden <branden.ackley@gmail.com>
2022-08-23 18:02:36 -0700

    Changed hash keys to ints and values to strings

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,11 +1,12 @@
+require 'pry-byebug'
+
 def caesar_cipher(string, shift_num)
   hash = {
-    A: 0, B: 1, C: 2, D: 3, E: 4, F: 5, G: 6, H: 7, I: 8, J: 9, K: 10,
-    L: 11, M: 12, N: 13, O: 14, P: 15, Q: 16, R: 17, S: 18, T: 19, U: 20,
-    V: 21, W: 22, X: 23, Y: 24, Z: 25
+    1 => 'A', 2 => 'B', 3 => 'C', 4 => 'D', 5 => 'E', 6 => 'F', 7 => 'G', 8 => 'H',
+    9 => 'I', 10 => 'J', 11 => 'K', 12 => 'L', 13 => 'M', 14 => 'N', 15 => 'O', 16 => 'P',
+    17 => 'Q', 18 => 'R', 19 => 'S', 20 => 'T', 21 => 'U', 22 => 'V', 23 => 'W', 24 => 'X',
+    25 => 'Y', 26 => 'Z'
   }
   string = string.upcase!
   array = string.split('')
-
-  string
 end
